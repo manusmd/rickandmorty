9aaef5dbce962a0a6ed2d3600f9ab73c3c0222e1
“manusmd” <manuel.schmid@js-tut.de>
2021-10-19 12:08:51 +0200

    ✨ Add app layout

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
 import './style.css';
+import { createElement } from './lib/element.js';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+function renderApp() {
+  const appElement = document.querySelector('#app');
+
+  const headerElement = createElement(
+    'header',
+    {
+      className: 'header',
+    },
+    [
+      createElement('img', {
+        className: 'rickmortyIMG',
+        src: 'assets/morty.png',
+        alt: '',
+      }),
+      createElement('h1', {
+        textContent: 'Rick and Morty',
+      }),
+      createElement('img', {
+        className: 'rickmortyIMG',
+        src: 'assets/rick.png',
+        alt: '',
+      }),
+    ]
+  );
+
+  const mainElement = createElement(
+    'main',
+    {
+      className: 'main',
+    },
+    [createElement('p', { textContent: 'In development...' })]
+  );
+
+  appElement.append(headerElement, mainElement);
+}
+renderApp();
